8472252ee635c8c34ab96c170cdf1e95b7bac96e
Vishal <samantro@github.com>
2022-09-16 10:37:31 +0530

    hexMarketplace first commit

---
+++ client/pages/create-and-list-nft.js
@@ -0,0 +1,179 @@
+import { useState } from 'react'
+import Web3 from 'web3'
+import Web3Modal from 'web3modal'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { useRouter } from 'next/router'
+
+//const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+const client = ipfsHttpClient('https://hexnft.infura-ipfs.io:5001/api/v0')
+
+import Marketplace from '../contracts/ethereum-contracts/Marketplace.json'
+import BoredPetsNFT from '../contracts/ethereum-contracts/BoredPetsNFT.json'
+
+// new stuff
+import axios from 'axios';
+import { contractAddress,PINATA_KEY,PINATA_SECRET } from './config';
+
+
+export default function CreateItem() {
+  const [fileUrl, setFileUrl] = useState(null)
+  const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' })
+  const router = useRouter()
+
+  async function onChange(e) {
+    // upload image to IPFS
+    const file = e.target.files[0]
+/*    try {
+      const added = await client.add(
+        file,
+        {
+          progress: (prog) => console.log(`received: ${prog}`)
+        }
+      )
+      //alert(" added path from onChange(): " + ${added.path})
+      const url = `https://hexnft.infura-ipfs.io/ipfs/${added.path}`
+      //const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      alert("url from onChange(): " + url)
+      setFileUrl(url)
+    } 
+*/
+     try{
+
+            const formData = new FormData();
+            formData.append("file",file);
+            const resFile = await axios({
+                method: "post",
+                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                data: formData,
+                headers:{
+                    'pinata_api_key':PINATA_KEY,
+                    'pinata_secret_api_key':PINATA_SECRET,
+                    'Content-Type':'multipart/form-data'
+                },
+            });
+            const url = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+            alert("url onChange "+ url);
+            //const ImageURL = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+            setFileUrl(url);
+    } //end of try
+
+    catch (error) {
+      console.log('Error uploading file: ', error)
+      alert ("!!!!! Error in on change - " + error)
+    }  
+  }
+
+   async function uploadToIPFS() {
+    const { name, description, price } = formInput
+    if (!name || !description || !price ) {
+      return
+    } else {
+      // first, upload metadata to IPFS
+      const data = JSON.stringify({
+        name, description, image: fileUrl
+      })
+      alert("uploadToIPFS:beofre TRY name,description,price  = " + name + " " + description + " " + price  + " " + fileUrl);
+      alert("uploadToIPFS:beofre TRY data = " + data);
+      //try {
+        //alert('faking ipfx')
+        /*const added = await client.add(data)
+        const url = `https://ipfs.infura.io/ipfs/${added.path}`
+        */
+        //const url = `https://ipfs.infura.io/ipfs/}`        
+        // after metadata is uploaded to IPFS, return the URL to use it in the transaction
+        //return url
+      //} 
+         try{
+
+            // const formData = new FormData();
+            // formData.append("file",file);
+            // const resFile = await axios({
+            //     method: "post",
+            //     url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+            //     data: formData,
+            //     headers:{
+            //         'pinata_api_key':PINATA_KEY,
+            //         'pinata_secret_api_key':PINATA_SECRET,
+            //         'Content-Type':'multipart/form-data'
+            //     },
+            // });
+            // const url = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+            // const ImageURL = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+            // setFileUrl(url);
+            alert("uploadToIPFS:end of TRY name,description,price  = " + name + " " + description + " " + price  + " " + fileUrl);
+            alert("uploadToIPFS:end of TRY data = " + data);
+            // alert("URL from uploadToIPFS -" + fileUrl)
+            return fileUrl;
+        } //end of try
+        catch (error) {
+        console.log('Error uploading file: ', error)
+        alert("Failure in uploadToIPFS -" + error)
+      } 
+    }
+  }
+
+  async function listNFTForSale() {
+    const web3Modal = new Web3Modal()
+    const provider = await web3Modal.connect()
+    const web3 = new Web3(provider)
+    const url = await uploadToIPFS()
+    const networkId = await web3.eth.net.getId()
+
+    // Mint the NFT
+    const boredPetsContractAddress = BoredPetsNFT.networks[networkId].address
+    const boredPetsContract = new web3.eth.Contract(BoredPetsNFT.abi, boredPetsContractAddress)
+    const accounts = await web3.eth.getAccounts()
+    const marketPlaceContract = new web3.eth.Contract(Marketplace.abi, Marketplace.networks[networkId].address)
+    let listingFee = await marketPlaceContract.methods.getListingFee().call()
+    listingFee = listingFee.toString()
+    alert("url line 69 - " + url)
+    alert("account used for listing " + accounts[0])
+    boredPetsContract.methods.mint(url).send({ from: accounts[0] }).on('receipt', function (receipt) {
+        console.log('minted');
+        // List the NFT
+        const tokenId = receipt.events.NFTMinted.returnValues[0];
+        marketPlaceContract.methods.listNft(boredPetsContractAddress, tokenId, Web3.utils.toWei(formInput.price, "ether"))
+            .send({ from: accounts[0], value: listingFee }).on('receipt', function () {
+                console.log('listed')
+                router.push('/')
+            });
+    });
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input 
+          placeholder="Asset Name"
+          className="mt-8 border rounded p-4"
+          onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+        />
+        <input
+          placeholder="Asset Price in Eth"
+          className="mt-2 border rounded p-4"
+          onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+        />
+        <input
+          type="file"
+          name="Asset"
+          className="my-4"
+          onChange={onChange}
+        />
+        {
+          fileUrl && (
+            <img className="rounded mt-4" width="350" src={fileUrl} />
+          )
+        }
+        <button onClick={listNFTForSale} className="font-bold mt-4 bg-teal-400 text-white rounded p-4 shadow-lg">
+          Mint and list NFT
+        </button>
+      </div>
+    </div>
+  )
+}
+
---
+++ client/pages/my-listed-nfts.js
@@ -0,0 +1,80 @@
+import Web3 from 'web3';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Web3Modal from 'web3modal';
+
+import Marketplace from '../../contracts/optimism-contracts/Marketplace.json';
+import BoredPetsNFT from '../../contracts/optimism-contracts/BoredPetsNFT.json';
+
+export default function CreatorDashboard() {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+
+  useEffect(() => { loadNFTs() }, [])
+
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal()
+    const provider = await web3Modal.connect()
+    const web3 = new Web3(provider)
+    const networkId = await web3.eth.net.getId()
+
+    // Get listed NFTs
+    const marketPlaceContract = new web3.eth.Contract(Marketplace.abi, Marketplace.networks[networkId].address)
+    const accounts = await web3.eth.getAccounts()
+    console.log("metamask accounts "+accounts[1]);
+    const listings = await marketPlaceContract.methods.getMyListedNfts().call({from: accounts[0]})
+    console.log("selected account "+accounts[0]);
+    console.log("my listed nfts = " + listings );
+    // Iterate over my listed NFTs and retrieve their metadata
+    const nfts = await Promise.all(listings.map(async i => {
+      //alert("iterator = " + i);
+      try {
+        //alert("loadNfts inside try : price,tokenId,seller....= " + i.price + " " + i.tokenId + " " + i.seller);
+        const boredPetsContract = new web3.eth.Contract(BoredPetsNFT.abi, BoredPetsNFT.networks[networkId].address)
+        const tokenURI = await boredPetsContract.methods.tokenURI(i.tokenId).call();
+        //alert("tokenUri inside the try"+tokenURI);
+        //const meta = await axios.get(tokenURI);
+        //alert("meta.data " + meta.data);
+        //alert("Nfts inside try: image url....= " + meta.data.image);
+        let item = {
+          price: i.price,
+          tokenId: i.tokenId,
+          seller: i.seller,
+          owner: i.owner,
+          //image: meta.data.image,
+          image: tokenURI,
+        }
+        return item
+      } catch(err) {
+        console.log(err)
+        return null
+      }
+    }))
+    setNfts(nfts.filter(nft => nft !== null))
+    setLoadingState('loaded')
+  }
+
+  if (loadingState === 'loaded' && !nfts.length) {
+    return (<h1 className="py-10 px-20 text-3xl">No NFTs listed</h1>)
+  } else {
+    return (
+      <div>
+        <div className="p-4">
+          <h2 className="text-2xl py-2">Items Listed</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {
+              nfts.map((nft, i) => (
+                <div key={i} className="border shadow rounded-xl overflow-hidden">
+                  <img src={nft.image} className="rounded" />
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl font-bold text-white">Price - {Web3.utils.fromWei(nft.price, "ether")} Eth</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
